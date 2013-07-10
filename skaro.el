27cd9d24e72acfa87e9fc5d04b7f759ae936c9a8
Phil Hagelberg <technomancy@gmail.com>
2013-07-09 22:09:11 -0700

    Fix elisp version.

--- skaro.el
+++ skaro.el
@@ -1,5 +1,79 @@
 ;;;; skaro.el --- Silly robots game
 
+;;; rules
+
+(defun skaro-killed-p ()
+  (member skaro-player (append skaro-enemies skaro-piles)))
+
+(defun skaro-allowed-p (x y)
+  (and (>= x 0) (>= y 0)
+       (<= x (car skaro-size)) (<= y (cdr skaro-size))))
+
+(defun skaro-collision-p (obstacles position)
+  (> (count position (append skaro-enemies skaro-piles)) 1))
+
+(defun skaro-get-collisions ()
+  (remove-if-not 'skaro-collision-p skaro-enemies))
+
+;;; drawing
+
+(defun skaro-place (marker position)
+  (goto-char (point-min))
+  (beginning-of-line (cdr position))
+  (forward-char (car position))
+  (delete-char 1)
+  (insert marker))
+
+(defun skaro-draw-board ()
+  (let (buffer-read-only)
+    (delete-region (point-min) (point-max))
+    (dotimes (n (cdr skaro-size))
+      (insert (make-string (car skaro-size) ?_) "\n"))
+    (skaro-place "O" skaro-player)
+    (dolist (enemy skaro-enemies)
+      (skaro-place "M" enemy))
+    (dolist (pile skaro-piles)
+      (skaro-place "X" pile))))
+
+;;; game commands
+
+(defun skaro-end (text)
+  (message text)
+  (fundamental-mode)
+  (read-only-mode t))
+
+(defun skaro-collide (collision)
+  (setq skaro-enemies (remove collision skaro-enemies))
+  (add-to-list 'skaro-piles collision))
+
+(defun skaro-move-enemy (enemy)
+  (let ((dx (- (car skaro-player) (car enemy)))
+        (dy (- (cdr skaro-player) (cdr enemy))))
+    (if (> (abs dx) (abs dy))
+        (cons (funcall (if (> dx 0) '1+ '1-) (car enemy)) (cdr enemy))
+        (cons (car enemy) (funcall (if (> dy 0) '1+ '1-) (cdr enemy))))))
+
+(defun skaro-play (x y)
+  (when (skaro-allowed-p (+ x (car skaro-player)) (+ y (cdr skaro-player)))
+    (setq skaro-player (cons (+ x (car skaro-player))
+                             (+ y (cdr skaro-player)))))
+  (setq skaro-enemies (mapcar 'skaro-move-enemy skaro-enemies))
+  (dolist (collision (skaro-get-collisions))
+    (skaro-collide collision))
+  (skaro-draw-board)
+  (when (skaro-killed-p)
+    (skaro-end "You died."))
+  (when (null skaro-enemies)
+    (skaro-end "You won. Nice job.")))
+
+(defun skaro-teleport ()
+  (intercative)
+  (let ((new-position (skaro-random)))
+    (skaro-play (- (car new-position) (car skaro-player))
+                (- (cdr new-position) (cdr skaro-player)))))
+
+;;; setup
+
 (defvar skaro-size '(10 . 10))
 (defvar skaro-enemies ())
 (defvar skaro-player ())
@@ -9,69 +83,24 @@
   (cons (random (car skaro-size))
         (random (cdr skaro-size))))
 
-(defun skaro-killed-p ()
-  (some (lambda (x) (equal x skaro-player)) (concat skaro-enemies skaro-piles)))
-
-(defun skaro-allowed-p (x y)
-  (and (> x 0) (> y 0)
-       (>= x (car skaro-size)) (>= y (cdr skaro-size))))
-
-(defun skaro-place (marker position)
-  (goto-char (point-min))
-  (beginning-of-line (cdr position))
-  (forward-char (car position))
-  (insert marker))
-
-(defun skaro-draw-board ()
-  (let (buffer-read-only)
-    (delete-region (point-min) (point-max))
-    (skaro-place "O" skaro-player)
-    (mapc (apply-partially 'skaro-place "M") skaro-enemies)
-    (mapc (apply-partially 'skaro-place "X") skaro-piles)))
-
-(defun skaro-collision-p (obstacles position)
-  (> (length (remove-if-not (apply-partially 'equal position) obstacles)) 1))
-
-(defun skaro-get-collisions ()
-  (remove-if-not (apply-partially 'skaro-collision-p
-                                  (concat skaro-enemies skaro-piles)
-                                  skaro-enemies)))
-
-(defun skaro-collide (collision)
-  (setq skaro-enemies (remove collision skaro-enemies))
-  (add-to-list 'skaro-piles collision))
-
-(defun move-enemy (enemy)
-  (let ((dx (- (car skaro-player) (car enemy)))
-        (dy (- (cdr skaro-player) (cdr enemy))))
-    (if (> (abs dx) (abs dy))
-        (cons ((if (> dx 0) 1+ 1-) (car enemy)) (cdr enemy))
-        (cons (car enemy) ((if (> dy 0) 1+ 1-) (cdr enemy))))))
-
-(defun skaro-move (x y)
-  (when (skaro-allowed-p (+ x (car skaro-player)) (+ y (cdr skaro-player)))
-    (setq skaro-player (cons (+ x (car skaro-player))
-                             (+ y (cdr skaro-player)))))
-  (setq skaro-enemies (mapcar 'skaro-move-enemy skaro-enemies))
-  (mapc 'skaro-collide (skaro-get-collisions))
-  (skaro-draw-board)
-  (when (skaro-killed-p)
-    (skaro-end "You died."))
-  (when (null skaro-enemies)
-    (skaro-end "You won. Nice job.")))
-
 (defvar skaro-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "<up>") (lambda () (interactive) (skaro-move 0 -1)))
-    (define-key map (kbd "<down>") (lambda () (interactive) (skaro-move 0 1)))
-    (define-key map (kbd "<left>") (lambda () (interactive) (skaro-move -1 0)))
-    (define-key map (kbd "<right>") (lambda () (interactive) (skaro-move 1 0)))
+    (define-key map (kbd "<up>") (lambda () (interactive) (skaro-play 0 -1)))
+    (define-key map (kbd "<down>") (lambda () (interactive) (skaro-play 0 1)))
+    (define-key map (kbd "<left>") (lambda () (interactive) (skaro-play -1 0)))
+    (define-key map (kbd "<right>") (lambda () (interactive) (skaro-play 1 0)))
+    (define-key map (kbd "SPC") 'skaro-teleport)
+    (define-key map (kbd "q") 'kill-this-buffer)
     map))
 
-(define-derived-mode skaro-mode fundamental-mode "skaro" "A game"
-  (set (make-local-variable 'skaro-enemies) (mapcar 'skaro-random (make-list 4 nil)))
+(define-derived-mode skaro-mode fundamental-mode "skaro" "A silly game."
+  (set (make-local-variable 'skaro-enemies) (mapcar 'skaro-random
+                                                    (make-list 4 nil)))
   (set (make-local-variable 'skaro-player) (skaro-random))
   (set (make-local-variable 'skaro-piles) ())
-  (set (make-local-variable 'buffer-read-only) t)
-  (overwrite-mode)
   (skaro-draw-board))
+
+(defun skaro ()
+  (interactive)
+  (switch-to-buffer "*skaro*")
+  (skaro-mode))
