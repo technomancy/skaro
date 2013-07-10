c12979667ad98ff3d361a6c36230857e6bd438da
Phil Hagelberg <technomancy@gmail.com>
2013-07-09 22:16:28 -0700

    Script to count lines.

---
+++ count.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# sloccount doesn't know about these extensions
+cp skaro.clj /tmp/skaro.lisp
+cp skaro.rkt /tmp/skaro-rkt.scm
+
+echo "Scheme:" $(sloccount skaro.scm | grep "of Code" | cut -f 2 -d =)
+echo "Racket:" $(sloccount /tmp/skaro-rkt.scm | grep "of Code" | cut -f 2 -d =)
+echo "Clojure:" $(sloccount /tmp/skaro.lisp | grep "of Code" | cut -f 2 -d =)
+echo "Emacs Lisp:" $(sloccount skaro.el | grep "of Code" | cut -f 2 -d =)
