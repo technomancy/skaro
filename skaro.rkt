e4eb9da8329dffbf78b6002d4f5855c6549569bc
Eli Barzilay <eli@barzilay.org>
2013-08-12 02:26:25 -0400

    Switch from a hash to a struct for board representation.
    In general, we try to avoid generic values like lists (as used in
    Scheme/Lisp) and hashes (as used in JS/Ruby/etc) when we have a specific
    type.

--- skaro.rkt
+++ skaro.rkt
@@ -3,6 +3,8 @@
 
 ;;;; SKARO: a game based on https://en.wikipedia.org/wiki/Robots_(BSD_game)
 
+(struct board (width height piles enemies player))
+
 ;;; rules
 
 (define (move position input)
@@ -13,10 +15,10 @@
     ['right (cons (add1 (car position)) (cdr position))]
     [_ #f]))
 
-(define (allowed? board position)
+(define (allowed? a-board position)
   (and position
-       (<= 0 (car position) (hash-ref board 'width))
-       (<= 0 (cdr position) (hash-ref board 'height))))
+       (<= 0 (car position) (board-width  a-board))
+       (<= 0 (cdr position) (board-height a-board))))
 
 (define (collision? obstacles position)
   (> (count (curry equal? position) obstacles) 1))
@@ -24,36 +26,36 @@
 (define (get-collisions enemies obstacles)
   (filter (curry collision? obstacles) enemies))
 
-(define (killed? board)
-  (member (hash-ref board 'player)
-          (append (hash-ref board 'enemies)
-                  (hash-ref board 'piles))))
+(define (killed? a-board)
+  (member (board-player a-board)
+          (append (board-enemies a-board)
+                  (board-piles   a-board))))
 
 ;;; drawing
 
 (define (place marker position rows)
   (hash-set rows position marker))
 
-(define (draw-board board)
-  (let* ([rows (hash (hash-ref board 'player) 'O)]
-         [rows (foldl (curry place 'M) rows (hash-ref board 'enemies))]
-         [rows (foldl (curry place 'X) rows (hash-ref board 'piles))])
-    (for ([y (hash-ref board 'height)])
-      (for ([x (hash-ref board 'width)])
+(define (draw-board a-board)
+  (let* ([rows (hash (board-player a-board) 'O)]
+         [rows (foldl (curry place 'M) rows (board-enemies a-board))]
+         [rows (foldl (curry place 'X) rows (board-piles   a-board))])
+    (for ([y (board-height a-board)])
+      (for ([x (board-width a-board)])
         (display (hash-ref rows (cons x y) '_)))
       (newline))))
 
 ;;; game loop
 
-(define (move-player board input)
+(define (move-player a-board input)
   (if (eq? input 'teleport)
-      (hash-set board 'player
-                (cons (random (hash-ref board 'width))
-                      (random (hash-ref board 'height))))
-      (let ([new-position (move (hash-ref board 'player) input)])
-        (if (allowed? board new-position)
-            (hash-set board 'player new-position)
-            board))))
+      (struct-copy board a-board
+                   [player (cons (random (board-width  a-board))
+                                 (random (board-height a-board)))])
+      (let ([new-position (move (board-player a-board) input)])
+        (if (allowed? a-board new-position)
+            (struct-copy board a-board [player new-position])
+            a-board))))
 
 (define (move-enemy player enemy)
   (define dx (- (car player) (car enemy)))
@@ -62,39 +64,39 @@
       (cons ((if (positive? dx) add1 sub1) (car enemy)) (cdr enemy))
       (cons (car enemy) ((if (positive? dy) add1 sub1) (cdr enemy)))))
 
-(define (move-enemies board)
-  (hash-update board 'enemies
-               (curry map (curry move-enemy (hash-ref board 'player)))))
+(define (move-enemies a-board)
+  (struct-copy board a-board
+               [enemies (map (curry move-enemy (board-player a-board))
+                             (board-enemies a-board))]))
 
-(define (collisions board)
-  (define collisions (get-collisions (hash-ref board 'enemies)
-                                     (append (hash-ref board 'enemies)
-                                             (hash-ref board 'piles))))
-  (define new-board (hash-update board 'piles (curry append collisions)))
-  (hash-update new-board 'enemies (curry remove* collisions)))
+(define (collisions a-board)
+  (define collisions (get-collisions (board-enemies a-board)
+                                     (append (board-enemies a-board)
+                                             (board-piles   a-board))))
+  (struct-copy board a-board
+               [piles   (append  collisions (board-piles   a-board))]
+               [enemies (remove* collisions (board-enemies a-board))]))
 
 (define round (compose collisions move-enemies move-player))
 
-(define (play board)
-  (draw-board board)
-  (cond [(killed? board)
+(define (play a-board)
+  (draw-board a-board)
+  (cond [(killed? a-board)
          (display "You died.\n")]
-        [(null? (hash-ref board 'enemies))
+        [(null? (board-enemies a-board))
          (display "You won. Nice job.\n")]
         [else (define input (read))
               (if (eq? input 'quit)
                   (display "Bye.\n")
-                  (play (round board input)))]))
+                  (play (round a-board input)))]))
 
 ;;; setup
 
 (define (make-board width height enemies)
   (define (rand-pos) (cons (random width) (random height)))
-  (hash 'width width
-        'height height
-        'piles '()
-        'enemies (for/list ([i enemies]) (rand-pos))
-        'player (rand-pos)))
+  (board width height '()
+         (for/list ([i enemies]) (rand-pos))
+         (rand-pos)))
 
 (module+ main
   (define width    10)
